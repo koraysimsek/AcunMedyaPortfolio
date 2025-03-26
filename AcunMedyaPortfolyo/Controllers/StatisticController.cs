74c246536484d036586ed2df060e04904b309dae
koray <koray_simsek05@hotmail.com>
2025-03-27 02:36:26 +0300

    Design

--- AcunMedyaPortfolyo/Controllers/StatisticController.cs
+++ AcunMedyaPortfolyo/Controllers/StatisticController.cs
@@ -20,6 +20,16 @@ namespace AcunMedyaPortfolyo.Controllers
             ViewBag.SkillCount = db.Skill.Count();
             ViewBag.ServicesCount = db.Services.Count();
             ViewBag.ProjectCount = db.Project.Count();
+            ViewBag.EducationCount = db.Education.Count();
+            ViewBag.JobCount = db.Job.Count();
+            ViewBag.LastProjectName = db.Project.OrderByDescending(p => p.ProjectID).FirstOrDefault()?.ProjectName;
+            ViewBag.LastServiceName = db.Services.OrderByDescending(p => p.ServicesID).FirstOrDefault()?.Title;
+            ViewBag.LastSkillName = db.Skill.OrderByDescending(p => p.SkillID).FirstOrDefault()?.SkillName;
+            ViewBag.LastTestimonialName = db.Testimonial.OrderByDescending(p => p.TestimonialID).FirstOrDefault()?.TestimonialName;
+            ViewBag.LastMessageName = db.Message.OrderByDescending(p => p.MessageID).FirstOrDefault()?.MessageContent;
+            ViewBag.LastJobName = db.Job.OrderByDescending(p => p.JobID).FirstOrDefault()?.Title;
+            ViewBag.LastCompanyName = db.Job.OrderByDescending(p => p.JobID).FirstOrDefault()?.CompanyName;
+            ViewBag.LastEducationName = db.Education.OrderByDescending(p => p.EducationID).FirstOrDefault()?.Name;
             return View();
         }
     }
